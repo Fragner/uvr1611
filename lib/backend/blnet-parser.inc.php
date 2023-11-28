fb86a5b557d4e6e258da8017bca116c7815256b3
Fragner <frama1038@gmail.com>
2023-11-28 14:03:47 +0100

    new calculateValue and modifidy converAnalag and codeformating

--- lib/backend/blnet-parser.inc.php
+++ lib/backend/blnet-parser.inc.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * Parses a binary string containing a dataset
- * 
+* Parses a binary string containing a dataset
+* 
  * Provides access to the values of a dataset as object properties
- *
- * @copyright  Copyright (c) Bertram Winter bertram.winter@gmail.com
- * @license    GPLv3 License
- */
+*
+* @copyright  Copyright (c) Bertram Winter bertram.winter@gmail.com
+* @license    GPLv3 License
+*/
 error_reporting(0);
-
 include_once("lib/config.inc.php");
 /* when it is too slow, Config must be a value from the constructor 
 * --> changes also in uvr1611-connection.inc.php necessary
@@ -17,14 +16,18 @@ include_once("lib/config.inc.php");
 class BlnetParser
 {
 	/**
-	 * Parser constant
-	 */
-	const SIGN_BIT = 0x8000;
-	const POSITIVE_VALUE_MASK = 0x00000FFF;
+	* Parser constant
+	*/
+	// 1 bit
 	const DIGITAL_ON = 1;
 	const DIGITAL_OFF = 0;
+	// 8 bit
 	const SPEED_ACTIVE = 0x80;
 	const SPEED_MASK = 0x1F;
+	// 16 bit
+	const INT16_POSITIVE_MASK = 0xFFFF;
+	const SIGN_BIT = 0x8000;
+	const POSITIVE_VALUE_MASK = 0x0FFF;
 	const TYPE_MASK = 0x7000;
 	const TYPE_NONE = 0x0000;
 	const TYPE_DIGITAL = 0x1000;
@@ -32,12 +35,13 @@ class BlnetParser
 	const TYPE_VOLUME = 0x3000;
 	const TYPE_RADIATION = 0x4000;
 	const TYPE_RAS = 0x7000;
-	const RAS_POSITIVE_MASK = 0x000001FF;
+	const RAS_POSITIVE_MASK = 0x01FF;
+	// 32 bit
 	const INT32_MASK = 0xFFFFFFFF;
 	const INT32_SIGN = 0x80000000;
-	
+	//RAS-Mode
 	const NEGATIVE_VALUE_MASK = 0xFFFFF000;
-	const RAS_NEGATIVE_MASK = 0xFFFFFE00;	
+	const RAS_NEGATIVE_MASK = 0xFFFFFE00; 
 	const RAS_NORMAL   = 0x200;
 	const RAS_LOWERING = 0x400;
 	const RAS_STANDBY  = 0x600;
@@ -45,15 +49,15 @@ class BlnetParser
 	const DEBUG = 0;
 	
 	/**
-	 * Constructor
-	 * Parses thourgh the dataset and add values as properties
-	 * @param string $data binary string containing the dataset
-	 */
+	* Constructor
+	* Parses thourgh the dataset and add values as properties
+	* @param string $data binary string containing the dataset
+	*/
 	public function __construct($data)
 	{
 		$configRASMode = Config::getInstance()->RASMode;
-		if (self::DEBUG ==1) {
-			echo "configRASMode: ";		
+		if (self::DEBUG > 3) {
+			echo "configRASMode: ";                     
 			echo "normal= ".$configRASMode->normal.", ";
 			echo "standby= ".$configRASMode->standby.", ";
 			echo "lowering= ".$configRASMode->lowering.", ";
@@ -62,110 +66,80 @@ class BlnetParser
 		}
 		// check if dataset contains time information
 		// (fetched from bootloader storage)
-		if(strlen($data) == 61) {
-			$package = unpack("C55/Cseconds/Cminutes/"
-					         ."Chours/Cdays/Cmonths/Cyears",$data);
-			$this->date = sprintf("20%02d-%02d-%02d %02d:%02d:%02d",
-					              $package["years"],   $package["months"],
-								  $package["days"],    $package["hours"],
-								  $package["minutes"], $package["seconds"]);
 
+		if(strlen($data) == 61) {
+			$package = unpack("C55/Cseconds/Cminutes/"."Chours/Cdays/Cmonths/Cyears",$data);
+			$this->date = sprintf("20%02d-%02d-%02d %02d:%02d:%02d" 
+								, $package["years"], $package["months"], $package["days"]
+								, $package["hours"], $package["minutes"], $package["seconds"]);
 		}
 		else {
-//	echo "datum should be added\n";
+//          echo "datum should be added\n";
 			$this->date = date("Y-m-d H:i:s");
-//	echo $this->date."\n";
-
+//          echo $this->date."\n";
 		}
 
 		// unpack binary string
-		$package = unpack("v16analog/vdigital/C4speed/Cactive".
-						  "/Vpower1/vkWh1/vMWh1/Vpower2/vkWh2/vMWh2",$data);
-		
+		$package = unpack("v16analog/vdigital/C4speed/Cactive/Vpower1/vkWh1/vMWh1/Vpower2/vkWh2/vMWh2",$data);
 		// 16 Analog channels
 		for($i=1; $i<=16; $i++) {
 			$key = ("analog".$i);
 			$this->$key = self::convertAnalog($package["analog".$i]);
 		}
-		
 		// 16 Digital channels (only 13 in use)
 		for($i=1; $i<=16; $i++) {
 			$key = ("digital".$i);
 			$this->$key = self::convertDigital($package["digital"],$i);
 		}
-		
 		// 4 speeds
 		for($i=1; $i<=4; $i++) {
 			$key = ("speed".$i);
 			$this->$key = self::convertSpeed($package["speed".$i]);
 		}
-		
 		// 2 energy values
 		for($i=1; $i<=2; $i++) {
 			$key = ("energy".$i);
-			$this->$key = self::convertEnergy($package["MWh".$i],
-											  $package["kWh".$i],
-											  $package["active"], $i);
+			$this->$key = self::convertEnergy($package["MWh".$i], $package["kWh".$i], $package["active"], $i);
 		}
-		
 		// 2 power values
 		for($i=1; $i<=2; $i++) {
 			$key = ("power".$i);
-			$this->$key = self::convertPower($package["power".$i],
-					                         $package["active"], $i);
+			$this->$key = self::convertPower($package["power".$i], $package["active"], $i);
 		}
-	
-	// 16 Analog channels check RAS-Mode
+		// 16 Analog channels check RAS-Mode
 		for($i=1; $i<=16; $i++) {
 			$key = ("RASMode".$i);
 			$this->$key = self::convertRASMode($package["analog".$i], $configRASMode);
-		}	
-	
+		}           
+
 	}
-	
+
 	/**
-	 * Provides access to the configuration properties
-	 * @param string $name Property name
-	 * @throws Exception Property not found
-	 */
+	* Provides access to the configuration properties
+	* @param string $name Property name
+	* @throws Exception Property not found
+	*/
 	public function __get($name)
 	{
 		throw new Exception('call to undefined property: '.$name);
 	}
-	
+
 	/**
-	 * Convert the int value to a float
-	 * @param int $value
-	 * @return number
-	 */
+	* Convert the int value to a float
+	* @param int $value
+	* @return number
+	*/
 	private static function convertAnalog($value)
-	{
-		
-	if (self::DEBUG > 1){
-		echo "convertAnalog:" .$value;
-		echo "\n";
-		}
-		
-		// calculate result value
-		if($value & self::SIGN_BIT) {
-			$result = $value | self::NEGATIVE_VALUE_MASK;
-			if (self::DEBUG >1) echo "negative value\n";
-		}
-		else {
-			$result = $value & self::POSITIVE_VALUE_MASK;
-			if (self::DEBUG >1) echo "positive value\n";			
-		}
+	{	
+		if (self::DEBUG > 1) echo "convertAnalog: ".$value."\n";
 		// choose type
 		switch($value & self::TYPE_MASK)
 		{
 			case self::TYPE_TEMP:
-				if (self::DEBUG >1) echo "TYPE_TEMP\n";
-				return $result/10;
+				return self::calculateValue($value, 0.1);
 			case self::TYPE_VOLUME:
-				if (self::DEBUG >1) echo "TYPE_VOLUME\n";			
-				return $result*4;
+				return self::calculateValue($value, 4);
 			case self::TYPE_DIGITAL:
-				if (self::DEBUG >1) echo "TYPE_DIGITAL\n";			
 				if($value & self::SIGN_BIT) {
 					return 1;
 				}
@@ -173,35 +147,23 @@ class BlnetParser
 					return 0;
 				}
 			case self::TYPE_RAS:
-				if (self::DEBUG >1) echo "TYPE_RAS\n";	
-				if($value & self::SIGN_BIT) {
-					$convValue = ($value | self::RAS_NEGATIVE_MASK)/10;
-					if (self::DEBUG ==1) echo "RAS_NEGATIVE_MASK: ".$convValue."\n";
-				//	return $convValue;
-					return (($value | self::RAS_NEGATIVE_MASK)/10);
-				}
-				else {
-					$convValue = ($value & self::RAS_POSITIVE_MASK)/10;					
-					if (self::DEBUG ==1) echo "RAS_POSITIVE_MASK - value: ".$value."\n";					
-					if (self::DEBUG ==1) echo "RAS_POSITIVE_MASK: ".$convValue."\n";
-//					return $convValue;					
-					return (($value & self::RAS_POSITIVE_MASK)/10);
-				}
+				return self::calculateValue($value, 0.1, self::RAS_POSITIVE_MASK);
 			case self::TYPE_RADIATION:
 			case self::TYPE_NONE:
 			default:
-				return $result;
+				return self::calculateValue($value);
 		}
 	}
-	
+
 	/**
-	 * Check if bit is set on a given position
-	 * @param int $value
-	 * @param int $position
-	 * @return number
-	 */
+	* Check if bit is set on a given position
+	* @param int $value
+	* @param int $position
+	* @return number
+	*/
 	private static function convertDigital($value, $position)
 	{
+		if (self::DEBUG > 1) echo "convertDigital: ".$value."\n";
 		if($value & (0x1<<($position-1))) {
 			return self::DIGITAL_ON;
 		}
@@ -209,14 +171,15 @@ class BlnetParser
 			return self::DIGITAL_OFF;
 		}
 	}
-	
+
 	/**
-	 * Check if speed is activated and returns its actual value
-	 * @param int  $value
-	 * @return NULL|boolean
-	 */
+	* Check if speed is activated and returns its actual value
+	* @param int  $value
+	* @return NULL|boolean
+	*/
 	private static function convertSpeed($value)
 	{
+		if (self::DEBUG > 1) echo "convertSpeed: ".$value."\n";
 		if($value & self::SPEED_ACTIVE) {
 			return "NULL";
 		}
@@ -224,88 +187,97 @@ class BlnetParser
 			return ($value & self::SPEED_MASK);
 		}
 	}
-	
+
 	/**
-	 * Checks if heat meter is activated on a given position
-	 * and returns its energy
-	 * @param int $value
-	 * @param int $active
-	 * @param int $position
-	 * @return number|NULL
-	 */
+	* Checks if heat meter is activated on a given position
+	* and returns its energy
+	* @param int $value
+	* @param int $active
+	* @param int $position
+	* @return number|NULL
+	*/
 	private static function convertEnergy($MWh, $kWh, $active, $position)
 	{
+		if (self::DEBUG > 1) echo "convertEnergy: ".$kWh."\n";
 		if($active & $position) {
-			return ($MWh*1000 + $kWh/10);
+			$kWh = self::calculateValue($kWh, 0.1, self::INT16_POSITIVE_MASK);
+			return ($MWh*1000 + $kWh);
 		}
 		else{
 			return "NULL";
 		}
 	}
-	
+
 	/**
-	 * Checks if heat meter is activated on a given position
-	 * and returns its power
-	 * @param int $value
-	 * @param int $active
-	 * @param int $position
-	 * @return number|NULL
-	 */
+	* Checks if heat meter is activated on a given position
+	* and returns its power
+	* @param int $value
+	* @param int $active
+	* @param int $position
+	* @return number|NULL
+	*/
 	private static function convertPower($value, $active, $position)
 	{
+		if (self::DEBUG > 1) echo "convertPower: ".$value."\n";
 		if($active & $position) {
-			if($value & self::INT32_SIGN) {
-				return -(($value ^ self::INT32_MASK)+1) / 2560;
-			}
-			else {
-				return ($value/2560);
-			}
+			return self::calculateValue($value, 1 / 2560, self::INT32_MASK, self::INT32_SIGN);
 		}
 		else {
 			return "NULL";
 		}
 	}
-	
+
 	/**
-	 * check if the input is a RAS and get the textual mode off the RAS
-	 * @param int $value, class $config
-	 * @return string
-	 	const RAS_NORMAL   = 0x200;
-	const RAS_LOWERING = 0x400;
-	const RAS_STANDBY  = 0x600;
-	 */
-	private static function convertRASMode($value, &$config)
+	* Calculate value with multiplier and positive mask
+	* @param int $value
+	* @param float $multiplier
+	* @param int $positivemask
+	* @return value
+	*/
+	private static function calculateValue($value, $multiplier = 1, $positivemask = self::POSITIVE_VALUE_MASK, $signbit = self::SIGN_BIT)
 	{
-		if (self::DEBUG ==2) {
-			echo "configRASMode: ";		
-			echo "normal= ".$config->normal.", ";
-			echo "standby= ".$config->standby.", ";
-			echo "lowering= ".$config->lowering.", ";
-			echo "timer= ".$config->timer.", ";
-			echo "notUsed= ".$config->notUsed."\n";
-	
-			echo "RASMODE value = ".$value." , ";	
+		if (self::DEBUG > 1) echo "calculateValue: ".$valu."\n";		
+		$result = $value & $positivemask;
+		if($value & $signbit) {
+			$result = -(($result ^ $positivemask) + 1);
 		}
+		return $result * $multiplier;
+		//return number_format('$result * $multiplier', 2);
+	}
+	/**
+	* check if the input is a RAS and get the textual mode off the RAS
+	* @param int $value, class $config
+	* @return string
+	* const RAS_NORMAL   = 0x200;
+	* const RAS_LOWERING = 0x400;
+	* const RAS_STANDBY  = 0x600;
+	* const TYPE_MASK = 0x7000;
+	* const TYPE_RAS = 0x7000;
+	*/
+	private static function convertRASMode($value, $config)
+	{
+		if (self::DEBUG > 1) echo "convertRASMode - RASMODE value = ".$value."\n"; 
 		// choose type
 		switch($value & self::TYPE_MASK)
 		{
 			case self::TYPE_RAS:
+				if (self::DEBUG > 1) echo "convertRASMode - TYPE_RAS\n"; 
 				if (($value & self::RAS_STANDBY) == self::RAS_STANDBY) {
-					if (self::DEBUG ==1) echo "standby = ".$config->standby."\n";			
+					if (self::DEBUG > 1 ) echo "standby = ".$config->standby."\n";
 					return $config->standby;
 				} else if (($value & self::RAS_LOWERING) == self::RAS_LOWERING) {
-					if (self::DEBUG ==1) echo "lowering = ".$config->lowering."\n";			
+					if (self::DEBUG > 1 ) echo "lowering = ".$config->lowering."\n";
 					return $config->lowering;
 				} else if(($value & self::RAS_NORMAL) == self::RAS_NORMAL) {
-					if (self::DEBUG ==1) echo "Normal = ".$config->normal."\n";			
-					return $config->normal;	
+					if (self::DEBUG > 1 ) echo "Normal = ".$config->normal."\n";
+					return $config->normal; 
 				} else {
-					if (self::DEBUG ==1) echo "timer = ".$config->timer."\n";			
+					if (self::DEBUG > 1 ) echo "timer = ".$config->timer."\n";
 					return $config->timer;
 				}
 			default:
-				return $config->notUsed;											
+				if (self::DEBUG > 1 ) echo "notUsed = ".$config->notUsed."\n"; 
+				return $config->notUsed;
 		}
 	}
-	
 }
